c50226dc50d4cde5ed99760e49ea51471909bbc6
AnewquisPURDUE <128609042+AnewquisPURDUE@users.noreply.github.com>
2023-04-04 22:57:59 -0400

    Update NN4a.R

--- NN4a.R
+++ NN4a.R
@@ -1,5 +1,7 @@
+#Use proper library
 library(neuralnet)
 
+#Import the data and clean it of Na's. Convert neg,pos to 0,1.
 aps_failure_data <- read.csv("aps_failure_training_set.csv", na.strings = c("","na"))
 aps_failure_data_clean <- na.omit(aps_failure_data)
 aps_failure_data_clean$class <- ifelse(aps_failure_data_clean$class == "pos", 1, 0)
@@ -7,21 +9,22 @@ aps_failure_data_clean$class <- as.numeric(aps_failure_data_clean$class)
 aps_failure_data_clean$aa_000 <- as.numeric(aps_failure_data_clean$aa_000)
 aps_failure_data_clean$ac_000 <- as.numeric(aps_failure_data_clean$ac_000)
 
+#Define training and testing data
 training_data <- aps_failure_data_clean[1:295, c("aa_000", "ac_000", "class")]
 testing_data <- aps_failure_data_clean[296:590, c("aa_000","ac_000", "class")]
 
 
-# Fit Neural Network
+# Creates the neural net with the training data
 neural_net4a <- neuralnet(class ~ aa_000 + ac_000, data = training_data,
                           hidden = 5, linear.output = FALSE, algorithm="rprop+")
 
 
-# Initialize variables to track errors during training and testing
+# Initialize variables to track errors
 training_errors <- rep(0, 10)
 testing_errors <- rep(0, 10)
 
 
-# Loop over the number of epochs
+# Loop over the number of epochs to collect errors
 for (i in 1:10) {
   # Compute errors on training data
   net_result_train <- compute(neural_net4a, training_data[, 1:2])
@@ -40,24 +43,14 @@ for (i in 1:10) {
                          training_data[, "class"])
 }
 
-#Plot the neural net
+#Plots the neural net
 par(mfrow = c(2,1))
 plot(neural_net4a,main = "Panel 1")
 
 
-# Plot the training and testing errors over the epochs
-plot(training_errors, type = "l", col = "blue", xlab = "Epoch #", 
-     ylab = "Errors", ylim = c(0, max(training_errors, testing_errors)))
-lines(testing_errors, type = "l", col = "red")
-legend("bottomright", legend = c("Training Errors", "Testing Errors"), 
-       col = c("blue", "red"), lty = 1)
-
-
-# Plot the training and testing errors over the epochs
+# Plots the training and testing errors over the epochs
 plot(training_errors, type = "l", col = "blue", xlab = "# of Epochs", 
      ylab = "# of Errors", ylim = c(0, max(training_errors, testing_errors)),main = "Panel 2")
 lines(testing_errors, type = "l", col = "red")
 legend("bottomright", legend = c("Training Errors", "Testing Errors"), 
        col = c("blue", "red"), lty = 1)
-
-
